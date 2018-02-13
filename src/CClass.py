7deca02fb1555a7c527335d0d8c5c50cc54f01ef
Henrik Rindlöw <henrik@rindlow.se>
2018-02-13 14:29:55 +0100

    Added GPL header

--- src/CClass.py
+++ src/CClass.py
@@ -1,3 +1,33 @@
+# CClass.py
+#
+# Copyright (C) 2017, 2018 Henrik Rindlöw
+#
+# This file is part of python-nettle.
+#
+# Python-nettle is free software: you can redistribute it and/or
+# modify it under the terms of either:
+#
+#   * the GNU Lesser General Public License as published by the Free
+#     Software Foundation; either version 3 of the License, or (at your
+#     option) any later version.
+#
+# or
+#
+#   * the GNU General Public License as published by the Free
+#     Software Foundation; either version 2 of the License, or (at your
+#     option) any later version.
+#
+# or both in parallel, as here.
+#
+# Python-nettle is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received copies of the GNU General Public License and
+# the GNU Lesser General Public License along with this program.  If
+# not, see http://www.gnu.org/licenses/.
+
 import re
 
 
